3e18a08d604b07cd381310583da1b66aeb384f92
Haile <haile4cmd@gmail.com>
2022-04-28 12:16:20 -0700

    error fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,10 +3,11 @@
 /**
  * _pow_recursion - function that returns the
  * value of x raised to the power of y
- * @x : the base
- * @y : the exponent
- * Return : x raised to the power of y
+ * @x: the base
+ * @y: the exponent
+ * Return: the value of x raised to the power of y
  */
+
 
 int _pow_recursion(int x, int y)
 {
