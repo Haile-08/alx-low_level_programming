0886320883ee5c066282e7820ef52a6053e98426
Haile <haile4cmd@gmail.com>
2022-04-28 11:02:28 -0700

    0 puts recutrsion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s:character to be printed
+ * Return:nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
