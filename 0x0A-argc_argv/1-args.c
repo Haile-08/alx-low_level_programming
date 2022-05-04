6d71faab5cfe63ca624d125c9e1f70873e335ce4
Haile-08 <haile4cmd@gmail.com>
2022-05-04 13:23:11 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)arcv;
+	printf("%i\n", arc - 1);
+
+	return (0);
+}
