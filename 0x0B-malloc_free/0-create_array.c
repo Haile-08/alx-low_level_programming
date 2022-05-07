496f18870fc170b7eb76b4c5dcbd7fb9aab02bfb
Haile-08 <haile4cmd@gmail.com>
2022-05-07 10:17:40 -0700

    fixed error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,18 +17,20 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
+	
 		return (NULL);
-	}
-	else
+
+	if(array == NULL)
+		return (NULL);
+
+	i = 0;
+	array = malloc(sizeof(char) * size);
+
+	while (i < size)
 	{
-		i = 0;
-		array = malloc(sizeof(char) * size);
-		while (i < size)
-		{
-			array[i] = c;
-			i++;
-		}
-		return (array);
+		array[i] = c;
+		i++;
 	}
+
+	return (array);
 }
