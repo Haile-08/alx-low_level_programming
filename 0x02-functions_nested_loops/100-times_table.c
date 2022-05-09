b006d5fa958f20ca8d893e561872687e4a9fb724
Haile-08 <haile4cmd@gmail.com>
2022-05-09 12:39:41 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 	int times;
+	int last;
 
 	if (n > 15 && n < 0)
 	{
@@ -20,11 +21,12 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 0; j < n; j++)
 			{
 				times = i * j;
 				printf("%d,   ", times);
 			}
+			printf("%d", i*n);
 			printf("\n");
 		}
 	}
