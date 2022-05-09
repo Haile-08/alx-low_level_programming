49ae9d1374799241c00c633408c0f72224cbd7e3
Haile-08 <haile4cmd@gmail.com>
2022-05-09 12:07:15 -0700

    fixed the errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,12 @@ void print_to_98(int n)
 	{
 		for (n = n; n < 98; n++)
 			printf("%d, ", n);
-		printf("%d", 98);
+		printf("%d\n", 98);
 	}
 	else
 	{
 		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-		printf("%d", 98);
+		printf("%d\n", 98);
 	}
 }
