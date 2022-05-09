dae71f2b3e2aed76bd2312f7ff9067109b82af5c
Haile-08 <haile4cmd@gmail.com>
2022-05-09 12:35:00 -0700

    betty fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,13 +18,14 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				times = i * j;
 				printf("%d, ", times);
 			}
+			printf("\n");
 		}
 	}
 }
