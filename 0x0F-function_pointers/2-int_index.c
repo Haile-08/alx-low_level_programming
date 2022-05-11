dd84e14fa7dfb2df17b623684bd1c6700dcf0826
Haile-08 <haile4cmd@gmail.com>
2022-05-11 12:31:08 -0700

    probelm 2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ *
+ * @array: the array
+ * @size: number of elements
+ * @cmp: pointer to the function
+ *
+ * Return: index of the first element
+ * 	if no match return -1
+ * 	size<=0 return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			
+		}
+	}
+
+	return (-1);
+
+}
