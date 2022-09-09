04922c420990208fa6e564a2e9a892b20dd9923a
Haile-08 <haile4cmd@gmail.com>
2022-09-09 09:58:32 -0700

    C program that print if a random generated  number is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			printf("%d is negative", n);
+		}
+		else
+		{
+			printf("%d is zero", n);
+		}
+	}
+	return (0);
+}
