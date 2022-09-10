1055abc18d9ebd1bfdc69fa2af9813bde504101c
Haile-08 <haile4cmd@gmail.com>
2022-09-10 08:32:35 -0700

    betty edit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,9 +4,9 @@
  * main - Entry point
  *
  * Description: print all possible different
- *		combinations of three digits
+ *		combinations of two digits.
  *
- * Retrun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
