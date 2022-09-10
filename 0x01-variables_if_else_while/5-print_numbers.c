8c71ba71063e817e1e9420b3b1c7ec9f4555008e
Haile-08 <haile4cmd@gmail.com>
2022-09-10 07:13:50 -0700

    Edit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Entry point
- * 
- * Description: print all single digit numbers
  *
- * Return: Always 0 (Sucess)
+ * Description: print all single digit number
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i = 0;
 
-	while (i <= 9)
+	while (i < 10)
 	{
 		printf("%d", i);
 		++i;
