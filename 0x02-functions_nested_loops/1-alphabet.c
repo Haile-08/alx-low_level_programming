f2e97814028c6371543b896624c614c15ff39f14
Haile-08 <haile4cmd@gmail.com>
2022-09-14 09:56:39 -0700

    Create a fucntion that prints the alphabet in lowercase followed by new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lowcase alphabet
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: null
+ */
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i < 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+
+
+void print_alphabet(void);
+
 #endif
