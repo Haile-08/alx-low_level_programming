e37019f7e87e136244ef38aa921b19a04baaa312
Haile-08 <haile4cmd@gmail.com>
2022-09-14 10:10:10 -0700

    Create a function that print the alphabet 10 times in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet 10x
+ *
+ * Description: print the alphabet 10x in
+ *		lowercase
+ *
+ * Return: null
+ */
+
+void print_alphabet_x10(void)
+{
+	int  i = 0;
+	char c;
+
+	while (i <= 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++;
+	}
+
+}
