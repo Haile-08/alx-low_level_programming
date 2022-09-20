c5c97799278973db6a9036092562a326ab761084
Haile-08 <haile4cmd@gmail.com>
2022-09-20 09:57:57 -0700

    Fix codding style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,11 @@ void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; ++index);
-
+	for (index = 0; s[index] != '\0'; ++index)
+		;
 	for (--index; index >= 0; --index)
+	{
 		_putchar(s[index]);
+	}
 	_putchar('\n');
 }
