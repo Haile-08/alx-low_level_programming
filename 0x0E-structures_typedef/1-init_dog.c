41d2a40aaa2077d63acca9e1d03682c67f653cd9
Haile-08 <haile4cmd@gmail.com>
2022-10-10 12:20:55 -0700

    Use different method of pointer to struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr;
-
-	ptr = d;
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define _DOG_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - dog description
