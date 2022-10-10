a44ece0425573492a15bc96ece4be1ff38a4a938
Haile-08 <haile4cmd@gmail.com>
2022-10-10 12:04:11 -0700

    Add a pointer to the function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	struct dog *ptr;
+
+	ptr = d;
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
 }
