c9eb0bdfe0bbf29a80dc565d1b99918a5d9c112e
Haile-08 <haile4cmd@gmail.com>
2022-10-10 11:35:51 -0700

    Create a struct dog with element name age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog description
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: a struct with a description of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
