b6eabcd116c3dd650f2d4274eea17394d90cb28a
Haile-08 <haile4cmd@gmail.com>
2022-10-11 05:24:16 -0700

    Edit the coding style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -56,7 +56,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
+
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
