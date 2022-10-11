70a16a08e1829a1e2dd5344f954c2f3e8ef7c14b
Haile-08 <haile4cmd@gmail.com>
2022-10-11 00:57:33 -0700

    If the element of d is null print (nil)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,11 @@
 
 void print_dog(struct dog *d)
 {
+	char n[6] = "(nil)";
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
+		printf("Name: %s\n", (d->name == NULL) ? n : d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", (d->owner == NULL) ? n : d->owner);
 	}
 }
