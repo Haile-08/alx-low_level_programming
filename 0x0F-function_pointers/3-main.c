08111c802c19814623ce8b7e9bc7913337a471f2
Haile-08 <haile4cmd@gmail.com>
2022-10-12 22:10:43 -0700

    Fix the Error message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,17 +9,31 @@
 
 int main(int argc, char *argv[])
 {
-	int (*get_op_func(char *s))(int, int);
-	if (argc == 4)
+	int num1, num2;
+	char *operator;
+
+	if (argc != 4)
 	{
-		if ((*argv[2] == '%' || *argv[2] == '/') && atoi(argv[3]) == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	exit(98);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
 }
