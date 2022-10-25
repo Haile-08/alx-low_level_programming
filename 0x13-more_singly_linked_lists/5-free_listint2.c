8d3a60d08292731a57878990f649b7a6fb91b1ff
Haile-08 <haile4cmd@gmail.com>
2022-10-25 01:34:04 -0700

    Remove the exit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,14 +14,12 @@ void free_listint2(listint_t **head)
 	listint_t *ptr;
 	listint_t *cur;
 	
-	if (*head == NULL)
-		exit(1);
 	ptr = *head;
-	*head = NULL;
 	while (ptr != NULL)
 	{
 		cur = ptr;
 		ptr = ptr->next;
 		free(cur);
 	}
+	*head = NULL;
 }
