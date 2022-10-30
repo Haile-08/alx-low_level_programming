c565016d43c3d6767643520e0dddc2b1bdfc4bf1
Haile-08 <haile4cmd@gmail.com>
2022-10-30 03:10:49 -0700

    Refactor the code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,31 +13,24 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *pre_ptr;
-	listint_t *post_ptr;
+	listint_t *ptr;
 	listint_t *new;
-	unsigned int i = 0;
 
-	pre_ptr = (*head);
-	while ((i < idx) && (pre_ptr->next != NULL))
-	{
-		pre_ptr = pre_ptr->next;
-		++i;
-	}
-	post_ptr = (*head);
-	i = 0;
-	while ((i < (idx - 1)) && (post_ptr->next != NULL))
-	{
-		post_ptr = post_ptr->next;
-		++i;
-	}
-	if (i < (idx - 1))
+	if (head == NULL)
 		return (NULL);
+	ptr = (*head);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = pre_ptr;
-	post_ptr->next = new;
+	new->next = NULL;
+
+	while (idx != 1)
+	{
+		ptr = ptr->next;
+		idx--;
+	}
+	new->next = ptr->next;
+	ptr->next = new;
 	return (new);
 }
