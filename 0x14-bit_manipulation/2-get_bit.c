65691ac5101975d8e0dd15a33a088baa74cf8bb4
Haile-08 <haile4cmd@gmail.com>
2022-11-04 01:23:11 -0700

    Create all the files

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit
+ * @n: the value
+ * @index: the index of the bit
+ *
+ * Description: find the value of a bit at a given index
+ *
+ * Return: the value of a bit at a given index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
