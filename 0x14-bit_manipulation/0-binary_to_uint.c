99c4439e90c4ea1df20adc95ef4e0c5113e9056f
Haile-08 <haile4cmd@gmail.com>
2022-11-04 00:28:04 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,6 @@
 #include "main.h"
 
 /**
- * _atoi - converts chars to integer
- *
- * @c: char to convert
- *
- * Return: converted integer
- */
-
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
- */
-
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
  * binary_to_uint - binary to decimal
  * @b: the binary input
  *
@@ -41,18 +11,18 @@ unsigned int _strlen(const char *str)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
+	unsigned int res = 0, i = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	while (b[i])
 	{
-		num = _atoi(b[index]);
-		if (num != 0 && num != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		result += num * base2;
-		base2 *= 2;
+		res <<= 1;
+		if (b[i] == '1')
+			res += 1;
+		i++;
 	}
-	return (result);
+	return (res);
 }
