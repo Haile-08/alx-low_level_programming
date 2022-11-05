5ce3e23bc776bffae399bc0cbbcf977f61157ab1
Haile-08 <haile4cmd@gmail.com>
2022-11-05 01:50:51 -0700

    Write a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - read the text file
+ * @filename: the file name
+ * @letters: the size of the letters
+ *
+ * Description: reads a text file and prints it to the POSIX standard output.
+ *
+ * Return: number of letters read
+ *	if the file is not opend return 0
+ *	if filename id NULL return 0
+ *	if wirte fails return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, rea, writ;
+	char *ch;
+
+	ch = malloc(sizeof(char) * letters);
+	if (filename == NULL || ch == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	rea = read(fd, ch, letters);
+	writ = write(1, ch, rea);
+	if (fd == -1 || writ == -1 || rea == -1)
+	{
+		free(ch);
+		return (0);
+	}
+	free(ch);
+	close(fd);
+	return (writ);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-typedef int ssize_t;
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+
+typedef long int ssize_t;
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
