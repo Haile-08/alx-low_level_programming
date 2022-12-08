4c5d7656ff1b0aaa016f237854979ff47c3ced38
Haile-08 <haile4cmd@gmail.com>
2022-12-08 03:57:29 -0800

    Remove the h variable and use head as is

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,25 +13,22 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	dlistint_t *h;
 
-	h = *head;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
-	if (h == NULL)
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (*head == NULL)
 	{
-		new->next = NULL;
-		new->n = n;
-		new->prev = NULL;
-		h = new;
+		*head = new;
 		return (new);
 	}
-	h->next = new;
-	new->prev = h;
-	new->next = NULL;
-	h = new;
+	new->next = *head;
+	(*head)->prev = new;
+	new->prev = NULL;
+	*head = new;
 	return (new);
 }
