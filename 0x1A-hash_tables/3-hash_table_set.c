88ed33b141debc25da64808498784e4829a37a31
Haile-08 <haile4cmd@gmail.com>
2023-01-12 04:21:17 -0800

    Fix code style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,41 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - create a node
+ * @key: input key
+ * @value: input value
+ *
+ * Description: create a new node
+ * Return: pointer to the new node
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
+}
+
+/**
  * hash_table_set - add an element
  * @ht: the table
  * @key: the key
@@ -27,11 +62,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp = ht->array[i];
 
 	/* create node */
-	new = malloc(sizeof(hash_node_t));
+	new = create_node(key, value);
 	if (new == NULL)
 		return (0);
-	new->value = strdup(value);
-	new->key = strdup(key);
 
 	/* check if hash table at index is empty */
 	if (ht->array[i] == NULL)
@@ -39,20 +72,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[i] = new;
 		return (1);
 	}
-	else
+	while (tmp)
 	{
-		while (tmp)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			if (strcmp(tmp->key, key) == 0)
-			{
-				tmp->value = strdup(value);
-				return (1);
-			}
-			tmp = tmp->next;
+			tmp->value = new->value;
+			return (1);
 		}
-		tmp->next = new;
-		new->next = NULL;
+		tmp = tmp->next;
 	}
+	tmp->next = new;
+	new->next = NULL;
 	return (1);
 
 }
