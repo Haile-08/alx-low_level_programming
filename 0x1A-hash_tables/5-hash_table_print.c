f3456c07220337a448eac7b889cfbf7fcbb36470
Haile-08 <haile4cmd@gmail.com>
2023-01-12 05:00:03 -0800

    Write a function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - table print
+ * @ht: input table
+ *
+ * Description: print the table
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+	char *comma = "";
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printf("{");
+	i = 0;
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			printf("%s'%s': '%s'", comma, tmp->key, tmp->value);
+			comma = ", ";
+			tmp = tmp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,6 +37,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 hash_node_t *hash_node_create(char *key, char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif
